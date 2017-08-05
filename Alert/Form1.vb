5871106294e4f62217a55f4c3724746c2c38219f
prison228 <hiryuu75@gmail.com>
2017-08-05 10:53:53 +0900

    ver1.00

--- Alert/Form1.vb
+++ Alert/Form1.vb
@@ -1,11 +1,10 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("テスト")
-    End Sub
-
-    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
         Dim dtToday As DateTime = DateTime.Today
-        'ComboBox3.Text And ComboBox1.Text And ComboBox2.Text = dtToday
-
+        Dim Today As String = ComboBox3.Text + "/" + ComboBox1.Text + "/" + ComboBox2.Text
+        If (dtToday = Today) Then
+            MsgBox(dtToday + TextBox1.Text)
+        End If
     End Sub
+
 End Class
